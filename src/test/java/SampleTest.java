86b0d4edaf3136b8f418b2a4d2727fd73df392ae
Sarath Dayala <sarathdayala@Saraths-iMac.local>
2023-03-29 13:14:53 -0600

    first commit

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class SampleTest {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+    @Test
+
+    public void Test1() throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://qavbox.github.io/demo");
+
+        Thread.sleep(3000);
+
+        System.out.println("Hello World");
+    }
+
 }
