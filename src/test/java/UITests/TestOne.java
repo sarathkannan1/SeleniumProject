0833b0ca092122fe52b75769c615b15c678d55a3
Sarath Dayala <sarathdayala@Saraths-iMac.local>
2023-04-04 14:26:01 -0600

    parameter for browser added

--- src/test/java/UITests/TestOne.java
+++ src/test/java/UITests/TestOne.java
@@ -4,22 +4,41 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class TestOne {
 
+        @Parameters("Browser")
         @Test
-        public void openQAVBox()
+        public void openQAVBox(String browsername)
         {
-            WebDriverManager.chromedriver().setup();
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--remote-allow-origins=*");
-            WebDriver driver = new ChromeDriver(options);
+            System.out.println("Parameter value is:" +browsername);
+            WebDriver driver = null;
+            if(browsername.contains("chrome"))
+            {
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+            }
+            else if(browsername.contains("firefox"))
+            {
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+
+            }
+            //WebDriverManager.chromedriver().setup();
+            //ChromeOptions options = new ChromeOptions();
+            //options.setHeadless(false);
+            //options.addArguments("--remote-allow-origins=*");
+            //WebDriver driver = new ChromeDriver(options);
+
+            //WebDriverManager.chromedriver().setup();
+            //WebDriver driver = new ChromeDriver();
             driver.get("https://qavbox.github.io/demo");
             Assert.assertTrue(driver.getCurrentUrl().contains("github"), "Does not match");
             driver.close();
         }
-
     }
 
